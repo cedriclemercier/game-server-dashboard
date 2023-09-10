3f40ccf6775c96b329336d5be471aa5542c3844c
cedriclemercier <info@cedricdesigns.com.au>
2023-09-10 20:56:14 +0700

    Added Railway config

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
 
-python manage.py generate-api -f
+# python manage.py generate-api -f
